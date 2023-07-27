473e37867bafa1570095096ce4f6afe028b1860d
eee595398 <eee595398@gmail.com>
2023-07-27 16:59:14 +0900

    1

---
+++ src/edu/kh/inheritance/model/vo/Person.java
@@ -0,0 +1,69 @@
+package edu.kh.inheritance.model.vo;
+
+public class Person extends Object {
+	//class명에 Object에대한 상속구문이 없다면 컴파일러가 자동으로 extends Object 구문을 추가해준다 
+	
+	
+
+	// 필드 
+	private String name; // 이름 
+	private int age; // 나이
+	private String nationality; // 국적
+	
+	//생성자
+	public Person() {}//기본 생성자
+	
+	//매개변수 생성자
+	public Person(String name, int age, String nationality) {
+		// 오보로딩 
+		//규칙 개수,순서,타입
+		this.name = name;
+		this.age = age;
+		this.nationality = nationality;
+		//this 참조변수
+	}
+	// 메서드 getter/setter
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getNationality() {
+		return nationality;
+	}
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+	
+	
+	public void breath() {
+		System.out.println("사람은");
+	}
+	
+	public void move() {
+		System.out.println("사람은 움질일 수 있다.");
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+		return name + "/" + age + "/" + nationality ;
+		
+	}
+	
+	
+}
---
+++ src/edu/kh/inheritance/run/InheritanceRun.java
@@ -0,0 +1,18 @@
+package edu.kh.inheritance.run;
+
+import edu.kh.inheritance.model.service.InheritanceServie;
+
+public class InheritanceRun {
+	
+	public static void main(String[] args) {
+		
+		InheritanceServie service = new InheritanceServie();
+		//service.ex1();
+		//service.ex2();
+		//service.ex3();
+		//service.ex4();
+		service.ex5();
+	}
+
+
+}
